087ca2475cdf171406b5756ea0190f3c5518e4ea
Abdishakuur1112 <eng.abdishakuur2022@gmail.com>
2024-03-03 14:39:25 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Index from './components/Index'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+  <div>
+    <Index/>
+  </div>
+  </div>
+)
